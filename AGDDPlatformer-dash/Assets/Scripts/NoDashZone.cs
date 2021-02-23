f3dfd597b4c177abbf6ad771474a8552220f79ae
Maniacswimmer <sanderpall@hotmail.com>
2021-02-23 17:36:48 +0000

    Created the NoDashZone prefab.

---
+++ AGDDPlatformer-dash/Assets/Scripts/NoDashZone.cs
@@ -0,0 +1,25 @@
+﻿using AGDDPlatformer;
+using UnityEngine;
+
+public class NoDashZone : MonoBehaviour
+{
+    public string playerTag;
+
+    private static void SetDashPermission(GameObject player, bool state)
+    {
+        PlayerController controller = player.GetComponent<PlayerController>();
+        controller.canDash = state;
+    }
+
+    private void OnTriggerEnter2D(Collider2D player)
+    {
+        if (player.CompareTag(playerTag))
+            SetDashPermission(player.gameObject, false);
+    }
+
+    private void OnTriggerExit2D(Collider2D player)
+    {
+        if (player.CompareTag(playerTag))
+            SetDashPermission(player.gameObject, true);
+    }
+}
